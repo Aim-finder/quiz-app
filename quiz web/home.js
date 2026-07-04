bcb31a5a8ac07cee5704fc3fa30b65ae0f412ebc
Aim-finder <sanamthakuri62@gmail.com>
2026-07-04 12:35:06 +0545

    Delete quiz web directory

--- quiz web/home.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('start').onclick = function () {
-  const count = document.getElementById('count').value;
-  const timer = document.querySelector('input[name="timer"]:checked').value;
-
-  localStorage.setItem('count', count);
-  localStorage.setItem('timer', timer);
-
-  window.location.href = "quiz.html";
-};
